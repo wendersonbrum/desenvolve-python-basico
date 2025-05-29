2494a73d16e6e082d1135d6ee7f85f22c4683f6d
wendersonbrum <163021629+wendersonbrum@users.noreply.github.com>
2025-05-29 20:34:36 -0300

    Delete modulo2 directory

--- modulo2/aula4_questao2.py
+++
@@ -1,8 +0,0 @@
-#leitura
-fahrenheit = int(input("Digite a temperatura em F"))
-
-#processamento
-celsius = ( fahrenheit - 32)* 5 /9
-
-#impressão
-print(f"{fahrenheit} graus Fahrenheit são {int(celsius)} graus Celsius.")
