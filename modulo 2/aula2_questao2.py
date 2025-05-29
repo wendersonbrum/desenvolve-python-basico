ae09cf4fa1b02ad572c7e9eee94c6b22f6a560ab
wendersonbrum <163021629+wendersonbrum@users.noreply.github.com>
2025-05-29 20:34:54 -0300

    Add files via upload

---
+++ modulo 2/aula2_questao2.py
@@ -0,0 +1,21 @@
+# Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+#Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+#Some os valores da segunda e terceira variável e armazene em outra variável.
+#Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+
+resultado = valor1 + valor2
+
+print(texto)
+print("Tipo da variável 'texto':", type(texto))
+
+print(valor1)
+print("Tipo da variável 'valor1':", type(valor1))
+
+print(valor2)
+print("Tipo da variável 'valor2':", type(valor2))
+
+print(resultado)
+print("Tipo da variável 'resultado':", type(resultado))
---
+++ modulo 2/aula4_questao2.py
@@ -0,0 +1,8 @@
+#leitura
+fahrenheit = int(input("Digite a temperatura em F"))
+
+#processamento
+celsius = ( fahrenheit - 32)* 5 /9
+
+#impressão
+print(f"{fahrenheit} graus Fahrenheit são {int(celsius)} graus Celsius.")
